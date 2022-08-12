a365459b63926bb999e56b558a5bef194e18b231
TOOMA1019 <fatimaaldosari.kwt@gmail.com>
2022-08-12 22:58:56 +0300

    cw2

---
+++ DAY 2 CW-2/DAY 2 CW-2/DAY2-CW22/DAY2-CW22/DAY2_CW22App.swift
@@ -0,0 +1,17 @@
+//
+//  DAY2_CW22App.swift
+//  DAY2-CW22
+//
+//  Created by fawziah hussain on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DAY2_CW22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TOOMA1/CW2.swift
@@ -0,0 +1,23 @@
+//
+//  CW2.swift
+//  TOOMA1 (iOS)
+//
+//  Created by fawziah hussain on 31/07/2022.
+//
+
+import SwiftUI
+
+struct CW2: View {
+    var body: some View {
+        ZStack{
+            Image("WhatsApp Image 2022-08-02 at 12.33.05 AM")
+        }
+    }
+}
+
+struct CW2_Previews: PreviewProvider {
+    static var previews: some View {
+        CW2()
+    }
+}
+
